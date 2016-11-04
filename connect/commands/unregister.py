5fc6872e2a52904c2c7478d03c1a63bc05d92ac4
juswilliams <justin.williams@rpdmsolutions.com>
2016-11-04 11:20:11 -0400

    fixed bug with unregistering connections

--- connect/commands/unregister.py
+++ connect/commands/unregister.py
@@ -13,12 +13,11 @@ class Unregister(BaseCommand):
 	def run(self):
 		from connect.models import GlobalConfig
 
-		config = GlobalConfig.load(self.options)
+		config = GlobalConfig.load()
 
 		if not hasattr(config, 'containers'):
 			print 'Configuration has no containers'
 			return
 			
 		config.containers.remove(self.options['CONNECTION_NAME'])
-
 		config.write()
